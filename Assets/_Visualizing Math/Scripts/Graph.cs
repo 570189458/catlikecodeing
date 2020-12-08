7be2d6b3d3b12b52d8cb5046979434abb13ca01a
tsundere <570189458@qq.com>
2020-12-08 18:39:10 +0800

    Visualizing Math

---
+++ Assets/_Visualizing Math/Scripts/Graph.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    [SerializeField]
+    private Transform pointPrefab = default;
+
+    [SerializeField, Range(10, 100)]
+    private int resolution = 10;
+
+    private Transform[] points;
+
+    private void Awake()
+    {
+        float step = 2f / resolution;
+        var position = Vector3.zero;
+        var scale = Vector3.one * step;
+        points = new Transform[resolution];
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform point = Instantiate(pointPrefab);
+            // point.localPosition = Vector3.right * ((i + 0.5f) / 5f - 1f);
+            position.x = (i + 0.5f) * step - 1f;
+            // position.y = position.x * position.x * position.x;
+            point.localPosition = position;
+            point.localScale = scale;
+            points[i] = point;
+            point.SetParent(transform, false);
+        }
+    }
+
+    private void Update()
+    {
+        float time = Time.time;
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            position.y = Mathf.Sin(Mathf.PI * (position.x + time));
+            point.localPosition = position;
+        }
+    }
+}
